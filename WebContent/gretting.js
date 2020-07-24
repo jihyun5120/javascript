72dedd1c8028f39edeac8e2ac0e6285be0f524d1
miso <miso@DESKTOP-4D610DM>
2020-07-24 09:17:01 +0900

    nomad coders 모맨텀

---
+++ WebContent/gretting.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    //유저가 없을 때
+  } else {
+    //유저가 있을 때
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
