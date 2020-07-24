bd789ce5cbcedab8a3c4167758bbaba20258db0e
miso <miso@DESKTOP-4D610DM>
2020-07-24 09:59:36 +0900

    UserName 저장

--- WebContent/gretting.js
+++ WebContent/gretting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function savename(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentvalue = input.value;
+  paintGreeting(currentvalue);
+  savename(currentvalue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -15,6 +31,7 @@ function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
     //유저가 없을 때
+    askForName();
   } else {
     //유저가 있을 때
     paintGreeting(currentUser);
